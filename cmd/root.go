2385c5b9aa1b3c959421a5acf86221f146d92236
mannk98 <khacman98@gmail.com>
2024-01-26 15:23:50 +0700

    init

---
+++ cmd/root.go
@@ -0,0 +1,99 @@
+/*
+Copyright © 2024 mannk khacman98@gmail.com
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/huntelaar112/goutils/utils"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile        string
+	Logger         = log.New()
+	LogLevel       = log.ErrorLevel
+	LogFile        = "minit.log"
+	cfgFileDefault = ".minit"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "minit",
+	Short: "A brief description of your application",
+	Long: `A longer description that spans multiple lines and likely contains
+examples and usage of using your application. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: rootRun,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	utils.InitLogger(LogFile, Logger, LogLevel)
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.minit.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		if err != nil {
+			Logger.Error(err)
+			os.Exit(1)
+		}
+
+		cfgFile = cfgFileDefault
+		// Search config in home directory with name ".minit" (without extension).
+		viper.AddConfigPath(home)
+		viper.AddConfigPath("./")
+		viper.SetConfigType("toml")
+		viper.SetConfigName(cfgFile)
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			log.Error("config.toml file at ./ folder is not exist. Create it first.")
+		} else {
+			Logger.Error(err)
+		}
+	} else {
+		Logger.Info("Using config file:", viper.ConfigFileUsed())
+	}
+}
+
+func rootRun(cmd *cobra.Command, args []string) {
+
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 mannk khacman98@gmail.com
+
+*/
+package main
+
+import "minit/cmd"
+
+func main() {
+	cmd.Execute()
+}
